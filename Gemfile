5ceb032e20958c7023f72e6ed00c738539e27972
Christian Silver <christian@decode.org.nz>
2015-04-10 11:11:51 +1200

    Add seeking

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "http://rubygems.org"
 
 gem 'jekyll', '3.0.0.pre.beta2'
+gem 'pygments.rb'
 gem 'redcarpet'
 gem 'classifier-reborn'
 gem 'fast-stemmer'
--- src/assets/js/video.js
+++ src/assets/js/video.js
@@ -5,27 +5,20 @@ var DecodeVideo = function(element) {
   this.videoId = element.data('videoId');
   this.transitions = element.data('transitions');
   this.isPlaying = false;
-
+  this.duration = 0;
   this.container = $('<div class="playback-container"></div>');
-  this.controls = $('<div class="video-controls"><div class="playback-button"></div><div class="seek-bar"><div class="seek-loaded"></div><div class="seek-needle"></div></div></div>');
+  this.controls = $('<div class="video-controls"><div class="playback-button"></div><div class="seek-container" touch-action="none"><div class="seek-bar"><div class="seek-loaded"></div><div class="seek-needle"></div></div></div></div>');
 
   this.element.append(this.container);
   this.element.append(this.controls);
 
   this.elements = {
     playback: this.controls.find('.playback-button'),
-    seek: this.controls.find('.seek-bar'),
+    seek: this.controls.find('.seek-container'),
     loaded: this.controls.find('.seek-loaded'),
     needle: this.controls.find('.seek-needle')
   };
 
-  this.elements.playback.click(function() {
-    if (this.isPlaying) {
-      this.pause();
-    } else {
-      this.play();
-    }
-  }.bind(this));
 
   // Async fun!
   if (this.videoId) {
@@ -49,13 +42,63 @@ var DecodeVideo = function(element) {
           iv_load_policy: '3'
         },
         events: {
-          onStateChange: this.onYTStateChange.bind(this)
+          onStateChange: this.onYTStateChange.bind(this),
+          onReady: function(event) {
+            this.duration = this.YTPlayer.getDuration();
+            this.ready();
+          }.bind(this)
         }
       });
     }.bind(this));
   } else {
     this.hasYT = false;
+    this.ready();
   }
+}
+
+// Private
+DecodeVideo.prototype.ready = function() {
+  this.elements.playback.click(function() {
+    if (this.isPlaying) {
+      this.pause();
+    } else {
+      this.play();
+    }
+  }.bind(this));
+
+  console.log(this.elements.seek);
+
+  var pointerMove = function(e) {
+    var parentOffset = this.elements.seek.offset();
+    var relX = e.pageX - parentOffset.left,
+        ratio = Math.max(0, Math.min(1, relX / this.elements.seek.width()));
+
+    this.setTime(ratio * this.duration, true);
+  }.bind(this);
+
+  var pointerUp = function(e) {
+    var parentOffset = this.elements.seek.offset();
+    var relX = e.pageX - parentOffset.left,
+        ratio = Math.max(0, Math.min(1, relX / this.elements.seek.width()));
+
+    this.setTime(ratio * this.duration);
+
+    this.elements.seek.off('pointerup', pointerUp);
+    this.elements.seek.off('pointermove', pointerMove);
+  }.bind(this);
+
+  this.elements.seek.on('pointerdown', function(e) {
+    console.log('?');
+    this.pause();
+
+    var parentOffset = this.elements.seek.offset();
+    var relX = e.pageX - parentOffset.left,
+        ratio = Math.max(0, Math.min(1, relX / this.elements.seek.width()));
+
+    this.setTime(ratio * this.duration, true);
+
+    this.elements.seek.on('pointerup', pointerUp).on('pointermove', pointerMove);
+  }.bind(this));
 }
 
 DecodeVideo.prototype.play = function() {
@@ -74,6 +117,30 @@ DecodeVideo.prototype.pause = function() {
   }
 };
 
+DecodeVideo.prototype.setTime = function(time, notunderlying, lookahead) {
+  if (this.hasYT) {
+    if (!notunderlying) {
+      this.YTPlayer.seekTo(time, lookahead || true);
+    }
+    this._setTime(time);
+  } else {
+  }
+};
+
+DecodeVideo.prototype._setTime = function(time) {
+  this.elements.needle.css({left: time / this.duration * 100 + '%'});
+};
+
+DecodeVideo.prototype.update = function() {
+  if (this.hasYT) {
+    console.log('update!');
+    this._setTime(this.YTPlayer.getCurrentTime());
+    this.elements.loaded.css("width", this.YTPlayer.getVideoLoadedFraction() * 100 + '%');
+  } else {
+    // ????
+  }
+};
+
 // Private
 DecodeVideo.prototype.setPlayState = function(state) {
   switch (state) {
@@ -81,12 +148,14 @@ DecodeVideo.prototype.setPlayState = function(state) {
       if (!this.isPlaying) {
         this.element.addClass('playing');
         this.isPlaying = true;
+        this.update.timer = setInterval(this.update.bind(this), 100);
       }
       break;
     case 'paused':
       if (this.isPlaying) {
         this.element.removeClass('playing');
         this.isPlaying = false;
+        clearInterval(this.update.timer);
       }
       break;
   }
@@ -136,4 +205,58 @@ DecodeVideo.loadYTAPI = function(callback) {
       break;
   }
 };
+
+
+/*    var pointerdown = function(e) {
+        scrubbing = true;
+
+        if (!audio.paused) {
+          audio.pause();
+        }
+
+        var parentOffset = elements.scrubContainer.offset();
+        //or $(this).offset(); if you really just want the current element's offset
+        var relX = e.pageX - parentOffset.left,
+            ratio = Math.max(0, Math.min(1, relX / elements.scrubContainer.width()));
+
+        setTime(audio.duration * ratio);
+
+
+        container.addClass('scrubbing');
+
+        $(document).on('pointermove', pointermove).on('pointerup', pointerup);
+
+        e.preventDefault();
+      };
+
+      var pointerup = function(e) {
+        if (scrubbing) {
+          scrubbing = false;
+          var parentOffset = elements.scrubContainer.offset();
+          //or $(this).offset(); if you really just want the current element's offset
+          var relX = e.pageX - parentOffset.left,
+              ratio = Math.max(0, Math.min(1, relX / elements.scrubContainer.width()));
+
+          if (audio.duration) {
+            audio.currentTime = audio.duration * ratio;
+            elements.needle.css({left: ratio * 100 + '%'});
+          }
+
+          $(document).off('pointermove', pointermove).off('pointerup', pointerup);
+          container.removeClass('scrubbing');
+
+          setTime(audio.duration * ratio);
+        }
+      };
+
+      var pointermove = function(e) {
+        if (scrubbing) {
+          var parentOffset = elements.scrubContainer.offset();
+          //or $(this).offset(); if you really just want the current element's offset
+          var relX = e.pageX - parentOffset.left,
+              ratio = Math.max(0, Math.min(1, relX / elements.scrubContainer.width()));
+
+          setTime(audio.duration * ratio);
+        }
+      };*/
 
